acd353dc2b9edab98348b472ecdb71859d74f49d
Felipe Vanderlei Brasil Pinheiro <felipe.vbpinheiro@gmail.com>
2018-06-28 19:19:36 -0300

    Obtendo um post por id.

---
+++ src/main/java/com/felipevanderlei/workshopmongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.felipevanderlei.workshopmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipevanderlei.workshopmongodb.domain.Post;
+import com.felipevanderlei.workshopmongodb.repository.PostRepository;
+import com.felipevanderlei.workshopmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
+	}
+}
