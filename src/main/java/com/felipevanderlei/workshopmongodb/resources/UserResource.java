d0ba370abdf156687688c7a2dea2b0397367790a
Felipe Vanderlei Brasil Pinheiro <felipe.vbpinheiro@gmail.com>
2018-06-28 19:08:04 -0300

    Endpoint para re tornar os posts de um usuário.

--- src/main/java/com/felipevanderlei/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/felipevanderlei/workshopmongodb/resources/UserResource.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.felipevanderlei.workshopmongodb.domain.Post;
 import com.felipevanderlei.workshopmongodb.domain.User;
 import com.felipevanderlei.workshopmongodb.dto.UserDTO;
 import com.felipevanderlei.workshopmongodb.services.UserService;
@@ -76,5 +77,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
