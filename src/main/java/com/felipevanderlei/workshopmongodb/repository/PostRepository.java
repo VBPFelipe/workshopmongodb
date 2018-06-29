7f07e40c41d5eeb38fb13aaef8aec31cd66052b1
Felipe Vanderlei Brasil Pinheiro <felipe.vbpinheiro@gmail.com>
2018-06-28 22:53:09 -0300

    Consulta Simples com Query Methods.

--- src/main/java/com/felipevanderlei/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/felipevanderlei/workshopmongodb/repository/PostRepository.java
@@ -1,9 +1,12 @@
 package com.felipevanderlei.workshopmongodb.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.felipevanderlei.workshopmongodb.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
-
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
