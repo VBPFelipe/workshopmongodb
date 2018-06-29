5bf890749c8fb54ded17d28a381eac39fe5583ec
Felipe Vanderlei Brasil Pinheiro <felipe.vbpinheiro@gmail.com>
2018-06-28 23:04:22 -0300

    Consulta simples com @Query.

--- src/main/java/com/felipevanderlei/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/felipevanderlei/workshopmongodb/repository/PostRepository.java
@@ -3,10 +3,15 @@ package com.felipevanderlei.workshopmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.felipevanderlei.workshopmongodb.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 	
+	@Query("{ 'title': { $regex: ?0, $options: 'i'} }")
+	List<Post> findByTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
